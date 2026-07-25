eb2d4fae16b0c1e116df2d07343137cc0782ac84
nileshns2007 <nileshns2007@gmail.com>
2026-07-25 08:59:46 +0530

    Handle equal numbers correctly

--- largestof3.c
+++ largestof3.c
@@ -16,10 +16,10 @@ int main(){
 }
 
 int largestof3(int a, int b, int c){
-    if (a>b && a>c){
+    if (a>=b && a>=c){
         return a;
     }
-    else if (b>a && b>c){
+    else if (b>=a && b>=c){
         return b;
     }
     else{
