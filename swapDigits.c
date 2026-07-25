cef2da2f9b97d17bb2274a6795853c5cfd8cfd96
nileshns2007 <nileshns2007@gmail.com>
2026-07-25 13:02:32 +0530

    Create fn to swap digits

---
+++ swapDigits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int swapDigits(int n);
+
+int main(){
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    printf("%d\n", swapDigits(n));
+
+    return 0;
+}
+
+int swapDigits(int n){
+    int t, o;
+    o=n%10;
+    t=(n-o)/10;
+    return 10*o+t;
+}
