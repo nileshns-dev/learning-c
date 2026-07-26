eb017576bcf02fb76b1d8cf7c2e36fc06227a2c3
nileshns2007 <nileshns2007@gmail.com>
2026-07-26 20:16:34 +0530

    Create fn to find the largest digit in a number

---
+++ largestDigit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int largestDigit(int n);
+
+int main(){
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("The largest digit is %d\n", largestDigit(n));
+    
+    return 0;
+}
+
+int largestDigit(int n){
+    int z;
+    for (int i=1; n>0; i++){
+        int a=n%10;
+        n/=10;
+        if (z>=a){
+            z=z;
+        }
+        else{
+            z=a;
+        }
+    }
+    return z;
+}
