06c7b959df8a45f72a4af35fcdd642dcae96d048
nileshns2007 <nileshns2007@gmail.com>
2026-07-31 17:58:51 +0530

    Print no.s in an inverted triangle pattern

--- numPattern.c
+++ numPattern.c
@@ -3,8 +3,8 @@
 int main(){
 
     int n=5;
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=i; j++){
+    for (int i=n; i>=1; i--){
+        for (int j=n; j>=i; j--){
             printf("%d", j);
         }
     printf("\n");
