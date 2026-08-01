d9e8763bfe49731981bf98389764fabf7973ae12
nileshns2007 <nileshns2007@gmail.com>
2026-08-01 11:32:03 +0530

    Print value of i from its pointer to pointer

--- pointers101.c
+++ pointers101.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int a=10;
-    int b=20;
-    int c=30;
-
-    printf("%d, %p\n", a, &a);
-    printf("%d, %p\n", b, &b);
-    printf("%d, %p\n", c, &c);
+    int i = 3;
+    int *ptr = i;
+    int **pptr = *(&ptr);
+    printf("%d", &pptr);
     return 0;
 }
