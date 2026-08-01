7b768a30d65cc16a6cfa94217fc5d104c1f38ccf
nileshns2007 <nileshns2007@gmail.com>
2026-08-01 22:53:57 +0530

    Create fn to calc sum, product & average of 2 numbers using pointers

--- pointers101.c
+++ pointers101.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+void ops(int a, int b, int *sum, int *pro, int *avg);
+
 int main(){
-    int i = 3;
-    int *ptr = i;
-    int **pptr = *(&ptr);
-    printf("%d", &pptr);
+    int a=3, b=5, sum, pro, avg;
+    ops(a, b, &sum, &pro, &avg);
+    printf("Sum is %d, product is %d and average is %d\n", sum, pro, avg);
     return 0;
+}
+
+void ops(int a, int b, int *sum, int *pro, int *avg){
+    *sum = a+b;
+    *pro = a*b;
+    *avg = (a+b)/2;
 }
