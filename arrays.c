940adf0cb144918952b34b1a9b583d6688a71c04
nileshns2007 <nileshns2007@gmail.com>
2026-08-02 18:21:59 +0530

    Count odd numbers in an array

--- arrays.c
+++ arrays.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
+int countOdd(int arr[], int n);
+
 int main(){
-    int marks[3];
-    printf("Enter Physics marks: ");
-    scanf("%d", &marks[0]);
-    printf("Enter Chem marks: ");
-    scanf("%d", &marks[1]);
-    printf("Enter Math marks: ");
-    scanf("%d", &marks[2]);
-
-    printf("P: %d, C: %d, M; %d\n", marks[0], marks[1], marks[2]);
-
+    int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    printf("%d\n", countOdd(arr, n));
+    
     return 0;
+}
+
+int countOdd(int arr[], int n){
+    int count=0;
+    for (int i=0; i<=n-1; i++){
+        if (arr[i]%2==1){
+            count+=1;
+        }
+        else{
+            count=count;
+        }
+    }
+    return count;
 }
